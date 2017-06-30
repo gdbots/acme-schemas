9ee0a6dd1d7e7d62e46935a0d26050c9407b6bd4
Greg Brown <greg@12dnetworks.com>
2017-06-30 01:22:47 -0700

    testing some js compiling (wip)

--- build/js/tests/Schema.test.js
+++ build/js/tests/Schema.test.js
@@ -1,6 +1,7 @@
 import test from 'tape';
 import Message from '@gdbots/pbj/Message';
 import MessageResolver from '@gdbots/pbj/MessageResolver';
+import '@gdbots/acme-schemas';
 
 test('Can create all messages', (t) => {
   MessageResolver.all().forEach((classProto) => {
