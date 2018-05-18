59f45165876f1faddff8b5b10a621e152b2445cc
Greg Brown <greg@12dnetworks.com>
2018-05-18 15:47:34 -0700

    remove app mixin from acme

--- build/php/src/Acme/Schemas/Iam/Node/App.php
+++
@@ -1,9 +0,0 @@
-<?php
-// @link http://acme-schemas.gdbots.io/json-schema/acme/iam/node/app/latest.json#
-namespace Acme\Schemas\Iam\Node;
-
-use Gdbots\Pbj\Message;
-
-interface App extends Message
-{
-}
